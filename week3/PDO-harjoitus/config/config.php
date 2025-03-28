22a545b4f25d026512578ad8cae0aecca6b72f34
hussainkazemian <hussain.kazemian@metropolia.fi>
2025-03-28 15:23:05 +0200

    Forms jne

---
+++ week3/PDO-harjoitus/config/config.php
@@ -0,0 +1,2 @@
+<?php
+$SITE_URL = 'http://localhost/mk-25-php/week3/PDO-harjoitus';
---
+++ week3/PDO-harjoitus/operations/selectData.php
@@ -0,0 +1,30 @@
+<?php
+global $DBH;
+require_once __DIR__ . '/../db/dbConnect.php';
+
+$sql = "SELECT MediaItems.*, Users.username FROM MediaItems JOIN Users ON Users.user_id = MediaItems.user_id;";
+
+try {
+    $STH = $DBH->query($sql);
+    $STH->setFetchMode(PDO::FETCH_ASSOC);
+    while ($row = $STH->fetch()):
+        ?>
+        <tr>
+            <td><?php echo $row['title']; ?></td>
+            <td><?php echo $row['description']; ?></td>
+            <td><?php echo date_format(date_create($row['created_at']), 'd.m.Y H:i'); ?></td>
+            <td><?php echo $row['username']; ?></td>
+            <td><img src="./uploads/<?php echo $row['filename']; ?>" alt="<?php echo $row['title']; ?>"></td>
+            <td>
+                <button class="modify-button" data-media_id="<?php echo $row['media_id']; ?>">Modify</button>
+                <button>Delete</button>
+            </td>
+        </tr>
+    <?php
+    endwhile;
+} catch (PDOException $error) {
+    echo "Could not select data from the database.";
+    file_put_contents(__DIR__ . '/../logs/PDOErrors.txt', 'selectData.php - ' . $error->getMessage(), FILE_APPEND);
+}
+
+
