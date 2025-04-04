c3914974741122f3e032bf87f26bb97091f463b7
hussainkazemian <hussain.kazemian@metropolia.fi>
2025-04-04 15:31:48 +0300

    added

--- week3/PDO-harjoitus/inc/update-form.php
+++ week3/PDO-harjoitus/inc/update-form.php
@@ -1,6 +1,6 @@
-<section id="update-media-item">
+<section id="insert-media-item">
     <!--suppress HtmlUnknownTarget -->
-    <form action="./operations/updateData.php" method="post">
+    <form action="./operations/insertData.php" method="post" enctype="multipart/form-data">
         <div class="form-control">
             <label for="title">Title:</label>
             <input type="text" name="title" id="title"/>
@@ -9,8 +9,10 @@
             <label for="description">Description:</label>
             <textarea name="description" id="description" rows="5"></textarea>
         </div>
-        <input type="hidden" name="user_id" value="1">
-        <input type="hidden" name="media_id" value="<?php echo $_GET['media_id']; ?>">
-        <button type="submit">Update</button>
+        <div class="form-control">
+            <label for="file">Select image to upload:</label>
+            <input type="file" name="file" id="file"/>
+        </div>
+        <button type="submit">Upload Image</button>
     </form>
 </section>
--- week3/PDO-harjoitus/operations/selectData.php
+++ week3/PDO-harjoitus/operations/selectData.php
@@ -1,6 +1,16 @@
 <?php
+if(session_status() != PHP_SESSION_ACTIVE) {
+    session_start();
+}
 global $DBH;
+global $SITE_URL;
+require_once __DIR__ . "/../config/config.php";
 require_once __DIR__ . '/../db/dbConnect.php';
+
+if (!isset($_SESSION['user'])) {
+    header('Location: '. $SITE_URL . '/user.php');
+    exit;
+}
 
 $sql = "SELECT MediaItems.*, Users.username FROM MediaItems JOIN Users ON Users.user_id = MediaItems.user_id;";
 
@@ -14,10 +24,17 @@ try {
             <td><?php echo $row['description']; ?></td>
             <td><?php echo date_format(date_create($row['created_at']), 'd.m.Y H:i'); ?></td>
             <td><?php echo $row['username']; ?></td>
-            <td><img src="./uploads/<?php echo $row['filename']; ?>" alt="<?php echo $row['title']; ?>"></td>
+            <td><img src="<?php echo $SITE_URL; ?>/uploads/<?php echo $row['filename']; ?>"
+                     alt="<?php echo $row['title']; ?>"></td>
             <td>
-                <button class="modify-button" data-media_id="<?php echo $row['media_id']; ?>">Modify</button>
-                <button>Delete</button>
+                <?php if ($_SESSION['user']['user_id'] == $row['user_id']
+                    || $_SESSION['user']['user_level_id'] == 1): ?>
+                    <button class="modify-button" data-media_id="<?php echo $row['media_id']; ?>">Modify</button>
+                    <!--suppress HtmlUnknownTarget -->
+                    <a href="<?php echo $SITE_URL; ?>/operations/deleteData.php?media_id=<?php echo $row['media_id']; ?>">Delete</a>
+                <?php else: ?>
+                    -
+                <?php endif; ?>
             </td>
         </tr>
     <?php
